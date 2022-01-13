5733de273aa8edf060ca790918bf2cb4dba345b9
pacho30000 <pcatapan@e3r2p5.42roma.it>
2022-01-13 16:12:01 +0100

    Fino a memcmp

---
+++ LIBFT/ft_memcmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/13 15:59:38 by pcatapan          #+#    #+#             */
+/*   Updated: 2022/01/13 16:11:23 by pcatapan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (((unsigned char *)s1)[i] == ((unsigned char *)s2)[i] && i < n)
+		i++;
+	if (i == n)
+		i--;
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+}

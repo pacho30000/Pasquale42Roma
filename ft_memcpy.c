4701c53c3ce74f87d142b899ef2bc7ffbb902f73
pacho30000 <pcatapan@e3r2p5.42roma.it>
2022-01-13 14:06:10 +0100

    Fino a strncmp

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 16:44:11 by pcatapan          #+#    #+#             */
+/*   Updated: 2022/01/13 13:59:48 by pcatapan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)src)[i] == ((unsigned char *)dst)[i];
+		i++;
+	}
+	return (dst);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 15:05:33 by pcatapan          #+#    #+#             */
+/*   Updated: 2022/01/13 14:04:39 by pcatapan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_strlcat(char *dst, const char *src, size_t destsize)
+{
+    size_t  i;
+    size_t  j;
+    size_t  x;
+    
+    i = 0;
+    while (dst[i] != '\0')
+        i++;
+    j = 0;
+    while (src[j] != '\0')
+        j++;
+    x = 0;
+    if (destsize <= i)
+        x = j + destsize;
+    else
+        x = j + i;
+    j = 0;
+    while (src[j] != '\0' && i + 1 < destsize)
+    {
+        dst[i++] = src[j++];
+    }
+    dst[i] = '\0';
+    return (x);
+}

1c330a43a23b233164f8483e9d02c8b868d797e5
pacho30000 <pcatapan@e3r2p5.42roma.it>
2022-01-20 14:02:15 +0100

    Modifiche su parte bonus

--- LIBFT/ft_lstadd_back.c
+++ LIBFT/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 14:47:41 by pcatapan          #+#    #+#             */
-/*   Updated: 2022/01/20 12:51:19 by pcatapan         ###   ########.fr       */
+/*   Updated: 2022/01/20 13:56:18 by pcatapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*node;
 
-	node = *lst;
-	if (!lst !new)
+	if (!lst || !new)
 		return ;
+	node = *lst;
 	if (*lst == NULL)
 	{
 		*lst = new;

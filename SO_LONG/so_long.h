48add9141ee120467eb23c922cd09609197c1dd2
pcatapan <pacho30000@gmail.com>
2022-03-09 08:20:30 +0100

    Start so long

--- SO_LONG/so_long.h
+++ SO_LONG/so_long.h
@@ -6,7 +6,7 @@
 /*   By: pcatapan <pcatapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 19:54:09 by pcatapan          #+#    #+#             */
-/*   Updated: 2022/03/03 20:13:02 by pcatapan         ###   ########.fr       */
+/*   Updated: 2022/03/09 07:48:23 by pcatapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 # define SO_LONG_H
 
 # include "minilibx/mlx.h"
-# include "PRINTF/ft_printf.h"
+# include "./PRINTF/ft_printf.h"
 # include "GET_NEXT_LINE/get_next_line.h"
 # include <unistd.h>
 # include <fcntl.h>
+
+typedef struct s_map
+{
+	int	h;
+	int	w;
+}	t_map;
 
 #endif
